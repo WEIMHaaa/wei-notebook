c56053b211db679792e055e14d41444f0d8d3204
1425615649 <1425615649@qq.com>
2022-04-02 17:02:59 +0800

    调整格式

--- linux/command/curl.sh
+++ linux/command/curl.sh
@@ -7,3 +7,7 @@ curl https://gitee.com/weimenghua/Learn-Java
 #2、curl -I url：获取请求页面或接口的请求头信息(get请求)
 curl -I https://github.com/WEIMHaaa/Learn-Java
 curl -I https://gitee.com/weimenghua/Learn-Java
+
+#3、curl -X POST：调用post接口
+curl -H "Content-Type:application/json" -X POST -d '{"id":"123456","username": "username123","password": "123456"}' 'http://127.0.0.1:9999/demo/add'
+
